a332b58761ada66707826983548a28d2cb251e98
Никита <nikitostokarev22@gmail.com>
2020-07-22 12:55:16 +0300

    Updates

---
+++ precache-manifest.4c8d4cf9851bd0ecb8903305fe5cdf13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06955326207de19920fa1c5cca510fe6",
+    "url": "/reactwarrior/react-project/index.html"
+  },
+  {
+    "revision": "42731e17a5ae5bae48b8",
+    "url": "/reactwarrior/react-project/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "96724e8ea1e88304fda0",
+    "url": "/reactwarrior/react-project/static/css/main.162123f6.chunk.css"
+  },
+  {
+    "revision": "42731e17a5ae5bae48b8",
+    "url": "/reactwarrior/react-project/static/js/2.d8640974.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactwarrior/react-project/static/js/2.d8640974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96724e8ea1e88304fda0",
+    "url": "/reactwarrior/react-project/static/js/main.49786b8f.chunk.js"
+  },
+  {
+    "revision": "b3f68be5092584c997f9",
+    "url": "/reactwarrior/react-project/static/js/runtime-main.dc163950.js"
+  }
+]);
